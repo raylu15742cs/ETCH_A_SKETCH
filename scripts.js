61031260c45ad7fd29cb6cbd926384084e35b8a4
raylu15742cs <raylu15742cs@gmail.com>
2021-10-06 16:45:30 -0700

    new-message

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 var caps = document.createElement('div');
 caps.id = 'container';
 caps.innerHTML ='<h1>ETCH - A - SKETCH</h1>';
-
 document.body.appendChild(caps);
+
+var button = document.createElement('button');
+button.id = 'choices';
+button.innerHTML = 'Clear the Grid';
+caps.appendChild(button);
 
 var boxes = document.createElement('div');
 boxes.id = 'box';
  numbox = 0;
 caps.appendChild(boxes);
+
 function makeBlocks(width) {
     var wides = ((960 - (2 * width)) / width) + "px";
     for (var i = 0; i < width; i++) {
@@ -24,13 +29,19 @@ function makeBlocks(width) {
         }                
         document.getElementById('box').appendChild(row);
     }
+    for (var i = 0; i < numbox; i++ ) {
+        const boxselect = document.getElementById(i)
+        boxselect.addEventListener('mouseover', function() {
+            boxselect.style.backgroundColor = "black";
+        })
+    }
 }
-
-makeBlocks(20);
+makeBlocks(16);
+//random color generator'#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6) //
 
 for (var i = 0; i < numbox; i++ ) {
-    const boxselect = document.getElementById(i)
-    boxselect.addEventListener('mouseover', function() {
-        boxselect.style.backgroundColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+        const clearbox = document.getElementById(i)
+        document.getElementById('choices').addEventListener('click', function() {
+            clearbox.style.backgroundColor = "white";
     })
 }
