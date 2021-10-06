7f779b0070126b00de1e8af0f8624b2d70e94eb8
raylu15742cs <raylu15742cs@gmail.com>
2021-10-06 14:18:54 -0700

    new-message

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var caps = document.createElement('div');
 caps.id = 'container';
-caps.innerHTML ='container';
+caps.innerHTML ='<h1>ETCH - A - SKETCH</h1>';
 
 document.body.appendChild(caps);
 
@@ -8,14 +8,16 @@ var boxes = document.createElement('div');
 boxes.id = 'box';
  
 caps.appendChild(boxes);
-
 function makeBlocks(width) {
+    var wides = ((960 - (2 * width)) / width) + "px";
     for (var i = 0; i < width; i++) {
         var row = document.createElement('div');
         row.className = "row";
         for (var j = 0; j < width; j++) {
             var box = document.createElement('div');
             box.className = "box";
+            box.style.height = wides;
+            box.style.width = wides;
             row.appendChild(box);
         }                
         document.getElementById('box').appendChild(row);
